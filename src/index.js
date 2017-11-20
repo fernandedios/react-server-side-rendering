685efebb06f506d62113dd665c4c4f209bfb0715
Fernan de Dios <info@mediagramstudios.com>
2017-11-20 14:08:30 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const exxpress = require('express');
+const React = require('react');
+const renderToString = require('react-dom/server').renderToString;
+
+const Home = require('./client/components/Home').default;
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  const content = renderToString(<Home />);
+
+  res.send(content);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
