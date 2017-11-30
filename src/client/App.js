f4b07d081a1a66b18b7a92000698fbea7a2a8e63
Fernan de Dios <info@mediagramstudios.com>
2017-11-30 11:42:14 +0800

    added authentication related action creator and reducer

--- src/client/App.js
+++ src/client/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { renderRoutes } from 'react-router-config';
 
+import Header from './components/Header';
+import { fetchCurrentUser } from './actions';
+
 // get all routes produced by the matchRoutes process as prop
 const App = ({ route }) => {
   // render child components using renderRoutes
   return (
     <div>
-      <h1>Header</h1>
+      <Header />
       {renderRoutes(route.routes)}
     </div>
   );
 };
 
-export default { component: App };
+export default {
+  component: App,
+  // destructure dispatch from store, then return result from dispatch method
+  loadData: ({ dispatch }) => dispatch(fetchCurrentUser()) 
+};
---
+++ src/client/reducers/authReducer.js
@@ -0,0 +1,11 @@
+import { FETCH_CURRENT_USER } from '../actions';
+
+export default function(state = null, action) {
+  switch(action.type) {
+    case FETCH_CURRENT_USER:
+      return action.payload.data || false; // explicitly return false if user is NOT authenticated
+
+    default:
+      return state;
+  }
+}
