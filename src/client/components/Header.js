5faf408777e71f3369433b518d398f25794718b2
Fernan de Dios <info@mediagramstudios.com>
2017-12-01 11:19:05 +0800

    wire up ath to header

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-export default () => {
+const Header = ({ auth }) => {
+  console.log(auth);
+
+  // show login or logout button depending on the auth value
+  // a tag to force full browser render
+  const authButton = auth ? (
+    <a href="/api/logout">Logout</a>
+  ) : (
+    <a href="/api/auth/google">Login</a>
+  );
+
   return (
-    <Link to="/">SSR</Link>
+    <div>
+      <Link to="/">SSR</Link>
+      <div>
+        <Link to="/users">Users</Link>
+        <Link to="/admins">Admins</Link>
+        {authButton}
+      </div>
+    </div>
   );
 };
+
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
